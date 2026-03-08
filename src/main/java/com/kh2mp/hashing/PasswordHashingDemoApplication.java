71efed37bf985ce0d246f0f722c52699c61c0f64
Hannie1812 <bichhang18122004@gmail.com>
2026-03-08 14:19:41 +0700

    Initial commit

---
+++ src/main/java/com/kh2mp/hashing/PasswordHashingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kh2mp.hashing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordHashingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordHashingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh2mp/hashing/PasswordHashingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh2mp.hashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordHashingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
